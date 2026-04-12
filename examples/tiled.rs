5d68c2ac7a5a1e2f127cd1fe6cec029500d19bea
fmeef <gnu3ra@riseup.net>
2026-04-12 15:11:34 -0500

    fix using fair offset

--- examples/tiled.rs
+++ examples/tiled.rs
@@ -7,15 +7,16 @@ use rand::Rng;
 fn main() {
     let r = rand::rng();
     let data: Vec<u8> = r.random_iter().take(16).collect();
-    let len = (data.len() * 2) as u32;
     let step = 8 as u32;
+    let len = (data.len() as u32 * step) / 4;
     let mut v = CellularAutomata::allocate(len, len);
 
     for x in 0..4 {
         for y in 0..4 {
-            let rule = data[x + 1 * y];
-            println!("rule={rule} offset={x},{y}");
-            v.rule_interlace(&[rule], &[data[x * y]])
+            let offset = (x * 4) + y;
+            let rule = data[offset];
+            println!("rule={rule} offset={offset}");
+            v.rule_interlace(&[rule], &[data[offset]])
                 .unwrap()
                 .offset(step * x as u32, step * y as u32)
                 .second(false)
